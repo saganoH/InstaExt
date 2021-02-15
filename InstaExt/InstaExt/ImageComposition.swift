e7233b9e2584260e414854db7b02085e78c7b11c
sagano <hikaru.sagano@jp.ricoh.com>
2021-02-15 14:41:49 +0900

    [task] card_174 編集画像の合成実装
    * 体裁整え
    * process以外のメソッドをextensionへ
    * 縦向き画像でも動くように

--- InstaExt/InstaExt/ImageComposition.swift
+++ InstaExt/InstaExt/ImageComposition.swift
@@ -5,17 +5,20 @@ class ImageComposition {
         guard let sourceImage = sourceImageView.image else {
             return sourceImageView.image
         }
-      
-        // 空白を含むマスク画像の生成
+        
+        // 余白を含むマスク画像の生成
         UIGraphicsBeginImageContext(filterImageView.frame.size)
         let context = UIGraphicsGetCurrentContext()!
         filterImageView.layer.render(in: context)
-        let maskedImage = UIGraphicsGetImageFromCurrentImageContext()
+        guard let maskedImage = UIGraphicsGetImageFromCurrentImageContext() else {
+            return sourceImage
+        }
         UIGraphicsEndImageContext()
-
-
-        let resizedMaskImage = resize(maskedImage: maskedImage!, sourceImage: sourceImage)
         
+        // マスク画像の余白を削除
+        let resizedMaskImage = maskedImage.cutout(sourceImage: sourceImage)
+        
+        // 元画像とマスク画像の合成処理
         UIGraphicsBeginImageContext(sourceImage.size)
         sourceImage.draw(in: CGRect(origin: CGPoint.zero, size: sourceImage.size))
         resizedMaskImage.draw(in: CGRect(origin: CGPoint.zero, size: sourceImage.size))
@@ -27,62 +30,65 @@ class ImageComposition {
         }
         return resultImage
     }
-    
-    func resize(maskedImage: UIImage, sourceImage: UIImage) -> UIImage {
-        if sourceImage.size.width > sourceImage.size.height {
-            let scale = sourceImage.size.width / maskedImage.size.width
-            let resizedImage = maskedImage.scaleImage(scaleSize: scale)
-            let spaceSize = (resizedImage.size.height - sourceImage.size.height ) / 2
-            let trimmingArea = CGRect(x: 0, y: spaceSize, width: sourceImage.size.width, height: sourceImage.size.height)
-            return resizedImage.cropping(to: trimmingArea)
-            
-        } else {
-            let scale = sourceImage.size.height / maskedImage.size.height
-            return maskedImage.scaleImage(scaleSize: scale)
-        }
-    }
-    
-    func trimmingImage(_ image: UIImage, trimmingArea: CGRect) -> UIImage {
-        UIGraphicsBeginImageContext(image.size)
-        image.draw(in: trimmingArea)
-        let drawedImage = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        return drawedImage!
-    }
 }
 
 extension UIImage {
-    // resize image
-    func reSizeImage(reSize:CGSize)->UIImage {
-        //UIGraphicsBeginImageContext(reSize);
-        UIGraphicsBeginImageContextWithOptions(reSize,false,UIScreen.main.scale);
-        self.draw(in: CGRect(x: 0, y: 0, width: reSize.width, height: reSize.height));
-        let reSizeImage:UIImage! = UIGraphicsGetImageFromCurrentImageContext();
-        UIGraphicsEndImageContext();
-        return reSizeImage;
+    func cutout(sourceImage: UIImage) -> UIImage {
+        let scale: CGFloat
+        let resizedImage: UIImage
+        let spaceSize: CGFloat
+        let trimmingArea: CGRect
+        
+        // 元画像の向きを判断しトリミングエリアを決定
+        if sourceImage.size.width > sourceImage.size.height {
+            scale = sourceImage.size.width / size.width
+            resizedImage = scaleImage(scaleSize: scale)
+            spaceSize = (resizedImage.size.height - sourceImage.size.height ) / 2
+            trimmingArea = CGRect(x: 0,
+                                  y: spaceSize,
+                                  width: sourceImage.size.width,
+                                  height: sourceImage.size.height)
+        } else {
+            scale = sourceImage.size.height / size.height
+            resizedImage = scaleImage(scaleSize: scale)
+            spaceSize = (resizedImage.size.width - sourceImage.size.width ) / 2
+            trimmingArea = CGRect(x: spaceSize,
+                                  y: 0,
+                                  width: sourceImage.size.width,
+                                  height: sourceImage.size.height)
+        }
+        return resizedImage.cropping(to: trimmingArea)
     }
-
-    // scale the image at rates
-    func scaleImage(scaleSize:CGFloat)->UIImage {
-        let reSize = CGSize(width: self.size.width * scaleSize, height: self.size.height * scaleSize)
-        return reSizeImage(reSize: reSize)
+    
+    func scaleImage(scaleSize: CGFloat) -> UIImage {
+        let reSize = CGSize(width: size.width * scaleSize, height: size.height * scaleSize)
+        
+        UIGraphicsBeginImageContextWithOptions(reSize, false, UIScreen.main.scale)
+        draw(in: CGRect(x: 0, y: 0, width: reSize.width, height: reSize.height))
+        guard let reSizeImage = UIGraphicsGetImageFromCurrentImageContext() else {
+            return self
+        }
+        UIGraphicsEndImageContext()
+        return reSizeImage
     }
     
     func cropping(to: CGRect) -> UIImage {
-            var opaque = false
-            if let cgImage = cgImage {
-                switch cgImage.alphaInfo {
-                case .noneSkipLast, .noneSkipFirst:
-                    opaque = true
-                default:
-                    break
-                }
+        var isOpaque = false
+        if let cgImage = cgImage {
+            switch cgImage.alphaInfo {
+            case .none, .noneSkipLast, .noneSkipFirst:
+                isOpaque = true
+            default:
+                break
             }
-
-            UIGraphicsBeginImageContextWithOptions(to.size, opaque, scale)
-            draw(at: CGPoint(x: -to.origin.x, y: -to.origin.y))
-            let result = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-            return result!
         }
+        
+        UIGraphicsBeginImageContextWithOptions(to.size, isOpaque, scale)
+        draw(at: CGPoint(x: -to.origin.x, y: -to.origin.y))
+        guard let result = UIGraphicsGetImageFromCurrentImageContext() else {
+            return self
+        }
+        UIGraphicsEndImageContext()
+        return result
+    }
 }
