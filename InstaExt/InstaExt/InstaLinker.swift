bd9d3441e31a03a2192d045287ecca26a79238e0
sagano <hikaru.sagano@jp.ricoh.com>
2021-02-18 17:12:45 +0900

    [story] card_146 インスタ連携機能実装
    * 初回に渡した画像が毎回表示されてしまう

--- InstaExt/InstaExt/InstaLinker.swift
+++ InstaExt/InstaExt/InstaLinker.swift
@@ -5,14 +5,32 @@ class InstaLinker {
         guard let jpg = image.jpegData(compressionQuality: 1.0) as NSData? else {
             return nil
         }
-        let path = NSTemporaryDirectory() + "tmpImage.jpg"
-        let url: URL = NSURL(string: path)! as URL
+        //let path = NSTemporaryDirectory() + "tmpImage.jpg"
+        //let url: URL = NSURL(string: path)! as URL
+        let url = FileManager.default.temporaryDirectory
+
 
         jpg.write(to: url, atomically: true)
 
         guard let instaUrl = URL(string: "instagram://library?LocalIdentifier=\(url)") else {
             return nil
         }
+
+        FileManager.default.clearTmpDirectory()
+        FileManager.default.clearTmpDirectory(path: url.path)
+
+        print(FileManager.default.fileExists(atPath: url.path))
+        if FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.removeItem(atPath: url.path)
+            } catch {
+                print("failed")
+            }
+        } else {
+            print("ファイルがないよ")
+        }
+        print(FileManager.default.fileExists(atPath: url.path))
+
         return instaUrl
     }
 
@@ -24,6 +42,34 @@ class InstaLinker {
                                       completionHandler: nil)
         } else {
             // インストールされていません　とアラート表示
+        }
+    }
+}
+
+extension FileManager {
+    func clearTmpDirectory() {
+        do {
+            let tmpDirURL = FileManager.default.temporaryDirectory
+            let tmpDirectory = try contentsOfDirectory(atPath: tmpDirURL.path)
+            try tmpDirectory.forEach { file in
+                let fileUrl = tmpDirURL.appendingPathComponent(file)
+                try removeItem(atPath: fileUrl.path)
+            }
+        } catch {
+           //catch the error somehow
+        }
+    }
+
+    func clearTmpDirectory(path: String) {
+        do {
+            let tmpDirURL = NSURL(string: path)! as URL
+            let tmpDirectory = try contentsOfDirectory(atPath: tmpDirURL.path)
+            try tmpDirectory.forEach { file in
+                let fileUrl = tmpDirURL.appendingPathComponent(file)
+                try removeItem(atPath: fileUrl.path)
+            }
+        } catch {
+           //catch the error somehow
         }
     }
 }
