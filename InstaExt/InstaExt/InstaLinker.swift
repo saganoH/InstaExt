48624b7d7cc8238dd120fabd55f7b7d34c0f84b5
sagano <hikaru.sagano@jp.ricoh.com>
2021-02-25 09:46:39 +0900

    [fix] card_146 インスタ連携機能実装
    * fetch失敗時のアラート追加

--- InstaExt/InstaExt/InstaLinker.swift
+++ InstaExt/InstaExt/InstaLinker.swift
@@ -2,7 +2,7 @@ import UIKit
 import Photos
 
 class InstaLinker: NSObject {
-
+    
     var delegate: InstaLinkerDelegate?
     
     func link(image: UIImage) {
@@ -16,14 +16,14 @@ class InstaLinker: NSObject {
         let fetchResult = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: fetchOptions)
         guard let lastAsset = fetchResult.lastObject,
               let urlScheme = URL(string: "instagram://library?LocalIdentifier=\(lastAsset.localIdentifier)") else {
-            let title = ""
-            let message = ""
+            let title = "Instagram連携エラー"
+            let message = "画像取得に失敗しました"
             let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             self.delegate?.failedToLink(with: alert)
             return
         }
-
+        
         if UIApplication.shared.canOpenURL(urlScheme) {
             UIApplication.shared.open(urlScheme)
         } else {
