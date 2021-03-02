16d9fa8891c12011234994c793afd1eced769ff2
sagano <hikaru.sagano@jp.ricoh.com>
2021-03-02 10:06:31 +0900

    [fix] card_184 画像が連続で取り込めてしまう問題対応
    * 重複コード削除

--- InstaExt/InstaExt/MainViewController.swift
+++ InstaExt/InstaExt/MainViewController.swift
@@ -106,25 +106,6 @@ class MainViewController: UIViewController {
             self.present(alert, animated: true)
         }
     }
-
-    private func showSecondTakeAlert() {
-        DispatchQueue.main.async {
-            let title = "新規画像取り込み"
-            let message = "編集中画像を破棄してもよろしいですか？"
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let okAction = UIAlertAction(title: "OK", style: .default) { (ok) in
-                alert.dismiss(animated: true, completion: nil)
-            }
-            let cancelAction = UIAlertAction(title: "CANCEL", style: .default) { (cancel) in
-                alert.dismiss(animated: true, completion: nil)
-            }
-
-            alert.addAction(cancelAction)
-            alert.addAction(okAction)
-            self.present(alert, animated: true)
-            return
-        }
-    }
 }
 
 // MARK: - ImageDeliveryクラスのDelegate
