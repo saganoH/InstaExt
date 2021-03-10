12d9f0dd51b8be45498c83e77a0ba6499f646be6
sagano <hikaru.sagano@jp.ricoh.com>
2021-03-10 14:06:15 +0900

    [story] card_207 顔認識ぼかし機能
    * 元画像とimageViewのアスペクト比を合わせて顔認識ぼかし

--- InstaExt/InstaExt/ViewController/EditorViewController.swift
+++ InstaExt/InstaExt/ViewController/EditorViewController.swift
@@ -2,14 +2,16 @@ import UIKit
 
 class EditorViewController: UIViewController {
 
-    @IBOutlet weak var sourceImageView: UIImageView!
-    @IBOutlet weak var filterImageView: UIImageView!
+    @IBOutlet weak var tmpView: UIView!
     @IBOutlet weak var toolView: UIView!
     @IBOutlet weak var toolSlider: UISlider!
     @IBOutlet weak var modeChanger: UISegmentedControl!
 
     var selectedFilter: FilterType?
     var sourceImage: UIImage?
+
+    private var sourceImageView: UIImageView?
+    private var filterImageView: UIImageView?
 
     private let faceDetection = FaceDetection()
     private var faces: [CGRect] = []
@@ -20,48 +22,34 @@ class EditorViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         faceDetection.delegate = self
-        
+
         // TODO: - モノクロの場合はスライダとmodeChangerの位置調整する
         prepareTool()
-        processFilter()
     }
     
     override func viewDidLayoutSubviews() {
-        guard filterImageView.mask == nil else {
-            return
+        if filterImageView == nil  {
+            makeImageViews()
+            processFilter()
         }
-        
-        maskView = MaskView(frame: filterImageView.frame)
-        guard let maskView = maskView else {
-            return
-        }
-        maskView.maskImageView.frame = filterImageView.bounds
-        view.addSubview(maskView)
-        filterImageView.mask = maskView.maskImageView
     }
     
     // MARK: - @IBAction
     
     @IBAction func changeModeAction(_ sender: UISegmentedControl) {
-//        guard let sourceImage = sourceImage else {
-//            return
-//        }
+        guard let sourceImage = sourceImage else {
+            return
+        }
+
         switch sender.selectedSegmentIndex {
         case 0:
             print("描画モード")
-            maskView?.addGestureRecognizer(UIPanGestureRecognizer(target: maskView, action: #selector(maskView?.panAction(_:))))
+            maskView?.addGestureRecognizer(UIPanGestureRecognizer(target: maskView,
+                                                                  action: #selector(maskView?.panAction(_:))))
         case 1:
             print("顔認識モード")
             maskView?.gestureRecognizers?.removeAll()
-
-            UIGraphicsBeginImageContextWithOptions(sourceImageView.frame.size, false, 0.0)
-            let context = UIGraphicsGetCurrentContext()!
-            sourceImageView.layer.render(in: context)
-            let tmpImage = UIGraphicsGetImageFromCurrentImageContext()
-            UIGraphicsEndImageContext()
-
-
-            faceDetection.request(image: tmpImage!)
+            faceDetection.request(image: sourceImage)
         default:
             fatalError("モードは2つのみ")
         }
@@ -78,8 +66,13 @@ class EditorViewController: UIViewController {
     }
     
     @objc func doneAction() {
+        guard let sourceImageView = sourceImageView,
+              let filterImageView = filterImageView else {
+            return
+        }
+
         let compositedImage = ImageComposition().process(source: sourceImageView,
-                                                       filter: filterImageView)
+                                                         filter: filterImageView)
         guard let resultImage = compositedImage,
               let navi = navigationController,
               let mainViewController = navi.viewControllers[(navi.viewControllers.count)-2] as? MainViewController else {
@@ -96,7 +89,6 @@ class EditorViewController: UIViewController {
             return
         }
         
-        sourceImageView.image = sourceImage
         navigationItem.title = selectedFilter.rawValue
         
         let cancelButtonItem = UIBarButtonItem(image: UIImage(systemName: "xmark"),
@@ -119,9 +111,42 @@ class EditorViewController: UIViewController {
         toolSlider.minimumValue = selectedFilter.min()
         toolSlider.value = selectedFilter.mid()
     }
+
+    private func makeImageViews() {
+        guard let sourceImage = sourceImage else {
+            return
+        }
+
+        sourceImageView = UIImageView(frame: tmpView.bounds.aspectFit(contentSize: sourceImage.size,
+                                                                      stretchble: true,
+                                                                      integer: true))
+        guard let sourceImageView = sourceImageView else {
+            return
+        }
+        sourceImageView.image = sourceImage
+        sourceImageView.center = tmpView.center
+        view.addSubview(sourceImageView)
+
+        filterImageView = UIImageView(frame: sourceImageView.frame)
+        guard let filterImageView = filterImageView else {
+            return
+        }
+        view.addSubview(filterImageView)
+
+        maskView = MaskView(frame: filterImageView.frame)
+        guard let maskView = maskView else {
+            return
+        }
+        maskView.maskImageView.frame = filterImageView.bounds
+        view.addSubview(maskView)
+
+        filterImageView.mask = maskView.maskImageView
+    }
     
     private func processFilter() {
-        guard let sourceImage = sourceImage, let selectedFilter = selectedFilter else {
+        guard let sourceImage = sourceImage,
+              let selectedFilter = selectedFilter,
+              let filterImageView = filterImageView else {
             return
         }
         filterImage = selectedFilter.filter().process(value: CGFloat(toolSlider.value),
@@ -140,14 +165,14 @@ extension EditorViewController: FaceDetectionDelegate {
     }
 
     func didGetFaces(faces: [CGRect]) {
-//        guard let sourceImage = sourceImage else {
-//            return
-//        }
+        guard let sourceImageView = sourceImageView else {
+            return
+        }
 
         // サイズを変換する
         let faces = convertRectsSize(sourceRects: faces, imageView: sourceImageView)
-
         print(faces)
+
         // 矩形表示はこれから
         // 顔ぼかし
         maskView?.drawCycle(faceBounds: faces)
@@ -168,3 +193,26 @@ extension EditorViewController: FaceDetectionDelegate {
     }
 }
 
+// MARK: - extension CGRect
+
+extension CGRect {
+    func aspectFit(contentSize: CGSize, stretchble: Bool, integer: Bool) -> CGRect {
+        let xZoom = width / contentSize.width
+        let yZoom = height / contentSize.height
+        let zoom = stretchble ? min(xZoom, yZoom) : min(xZoom, yZoom, 1)
+
+        if integer {
+            let newWidth = max(Int(contentSize.width * zoom), 1)
+            let newHeight = max(Int(contentSize.height * zoom), 1)
+            let newX = Int(origin.x) + (Int(width) - newWidth) / 2
+            let newY = Int(origin.y) + (Int(height) - newHeight) / 2
+            return CGRect(x: newX, y: newY, width: newWidth, height: newHeight)
+        } else {
+            let newWidth = contentSize.width * zoom
+            let newHeight = contentSize.height * zoom
+            let newX = origin.x + (width - newWidth) / 2
+            let newY = origin.y + (height - newHeight) / 2
+            return CGRect(x: newX, y: newY, width: newWidth, height: newHeight)
+        }
+    }
+}
